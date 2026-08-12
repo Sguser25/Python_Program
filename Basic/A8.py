72d812eb9879b80e219cb107f2f56efece5adc1e
Sguser25 <87290459+Sguser25@users.noreply.github.com>
2026-08-12 18:15:31 +0530

    Update

--- Basic/A8.py
+++ Basic/A8.py
@@ -3,6 +3,6 @@ Write a program that:
 Takes an integer as input from the user.
 Prints the square and cube of that number
 '''
-n1 = int(input("Enter a number:")
+n1 = int(input("Enter a number:"))
 print("Square is = ",n1**2)
 print("Cube is = ",n1**3)
