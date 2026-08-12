3721086f5e8894ab5164c0a687f15749c3fbb0ab
Sguser25 <87290459+Sguser25@users.noreply.github.com>
2026-08-12 17:10:50 +0530

    Create

---
+++ Basic/A3.py
@@ -0,0 +1,13 @@
+'''
+Create variables to store: - Your name (string)
+- Your age (integer)
+- Your height in meters (float)
+- A boolean value representing whether you are a student
+Print all of them in one line.
+'''
+
+name = "Snigdha"
+age = 30
+height = 5.2
+is_student = True
+print("The name of the student is " + name + ". She is " + str(age) + " years old. " + "She is " + str(height) + " feet tall. " + "Student: " + str(is_student))
