cf80c7174a5519c8465247de35a2ef03828dc995
Sguser25 <87290459+Sguser25@users.noreply.github.com>
2026-08-12 17:22:29 +0530

    Create

---
+++ Basic/A5.py
@@ -0,0 +1,8 @@
+'''
+Write a program that:
+Asks the user for their favorite food.
+Prints:
+Wow! I also like <food>.
+'''
+food = input("Enter your favourite food:")
+print("Wow! I also like ",food)
