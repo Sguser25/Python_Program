aca0ef69dc40f464527a053b23ad34cacb72574c
Sguser25 <87290459+Sguser25@users.noreply.github.com>
2026-08-12 17:30:09 +0530

    Create

---
+++ Basic/A6.py
@@ -0,0 +1,11 @@
+'''
+Write a program that:
+Takes two numbers as input from the user.
+Prints their sum, difference, product, and quotient.
+'''
+A = int(input("Enter first number:"))
+B = int(input("Enter second number:"))
+print("Sum is = ",A+B)
+print("Difference is = ",A-B)
+print("Product is = ",A*B)
+print("Quotient is = ",A/B)
