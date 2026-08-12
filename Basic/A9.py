35f82d34fcaed78b1e56e7a044fe1bc595329c9b
Sguser25 <87290459+Sguser25@users.noreply.github.com>
2026-08-12 18:33:02 +0530

    A9.py

---
+++ Basic/A9.py
@@ -0,0 +1,8 @@
+# Write a program that asks the user for a number and prints whether it is positive, negative, or zero.
+A = float(input("Enter a number:"))
+if (A>0):
+  print("Positive")
+elif(A<0):
+  print("Negative")
+else:
+  print("Zero")
